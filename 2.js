d038d8f88a6800aac989b91f435752796c039763
darksh3ll <energie.sud83@gmail.com>
2018-06-14 20:05:10 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ 2.js
@@ -0,0 +1,21 @@
+// function maxMinscore(arr) {
+//     return Math.max(...arr) ;
+
+// }
+
+// function point(score) {
+//     arr.push(score)
+// }
+// const arr = [1,2,3,18,87,12,0,233]
+// point(800);
+// console.log(maxMinscore(arr));
+let compteur = 1;
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+    compteur++;
+    if (compteur === 10) {
+        window.refr
+    }
+
+    console.log(compteur);
+})
