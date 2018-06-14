2f425b2ec06eb827c788fb52318e8a9570f3dda5
darksh3ll <energie.sud83@gmail.com>
2018-06-14 20:22:55 +0200

    ddd

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const titres = document.querySelector("h1");
 const icone = document.querySelector('.fas')
+const button = document.querySelector('button');
+
+button.addEventListener('click',function () {
+    button.style.fontSize=('78px');
+})
 
 
 
