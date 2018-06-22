977d872b23baae32644943c421464ff4792876f9
darksh3ll <energie.sud83@gmail.com>
2018-06-22 18:02:11 +0200

    bnn

---
+++ 4.js
@@ -0,0 +1,19 @@
+const arr = [1,2,5,6,8,1]
+
+function double(arr) {
+for (let nombres of arr ) {
+    console.log(nombres);
+}
+};
+
+let resultat = [];
+for (let i = 0; i <= 6; i++) {
+    const chifAleat = Math.round(Math.random()*10);
+
+
+}
+
+double(arr)
+
+
+
